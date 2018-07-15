c02640236363d3d475a77220d06e92bcfa390b35
Requi <privat@requi-dev.de>
2018-07-15 18:17:51 +0200

    Relevant code in error message.

--- zen-external/main.cpp
+++ zen-external/main.cpp
@@ -20,14 +20,24 @@ int main(int argc, char* argv[])
 		int last_error(base::get_last_error());
 		HRESULT last_hresult(base::get_last_hresult());
 		NTSTATUS last_ntstatus(base::get_last_ntstatus());
+		int relevant_code(0);
 		if (last_error)
+		{
 			error_message = base::get_error_message(last_error);
+			relevant_code = last_error;
+		}
 		else if (last_hresult)
+		{
 			error_message = base::get_error_message(last_hresult);
+			relevant_code = last_hresult;
+		}
 		else if (last_ntstatus)
+		{
 			error_message = base::get_nt_message(last_ntstatus);
+			relevant_code = last_ntstatus;
+		}
 
-		std::cout << "Something went wrong: " << error_codes[return_code - 1000] << (error_message.empty() ? "" : "\n(" + std::to_string(last_error) + ") ") << error_message << std::endl;
+		std::cout << "Something went wrong: " << error_codes[return_code - 1000] << (error_message.empty() ? "" : "\n(" + std::to_string(relevant_code) + ") ") << error_message << std::endl;
 		std::cin.get();
 	}
 	return return_code;
